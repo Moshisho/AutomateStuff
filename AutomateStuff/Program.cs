24f17e02d05c963d8059dc244279ae8c9ffee5c9
Moshisho <moshisho84@gmail.com>
2014-12-15 15:42:41 +0200

    Added projects from work PC

---
+++ AutomateStuff/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomateStuff
+{
+    class Program : IProgram
+    {
+        static void Main(string[] args)
+        {
+            
+
+        }
+        public string IProgram.Name
+        {
+            get { return "StamProg"; }
+        }
+        public object IProgram.Run(object obj) 
+        {
+            return obj;
+        }
+    }
+}
---
+++ UIAutomationTry/GetWindowsHandler.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+
+
+namespace UIAutomationTry
+{
+    public class GetWindowsHandler
+    {
+        public AutomationElementCollection GetAllDesktopWindows() 
+        {
+            AutomationElementCollection winCollection = AutomationElement.RootElement.FindAll(TreeScope.Children, Condition.TrueCondition);
+
+            foreach (AutomationElement ele in winCollection) 
+            {
+                Console.WriteLine("--------Element");
+                Console.WriteLine("AutomationId: {0}", ele.Current.AutomationId);
+                Console.WriteLine("Name: {0}", ele.Current.Name);
+                Console.WriteLine("ClassName: {0}", ele.Current.ClassName);
+                Console.WriteLine("ControlType: {0}", ele.Current.ControlType.ProgrammaticName);
+                Console.WriteLine("IsEnabled: {0}", ele.Current.IsEnabled);
+                Console.WriteLine("IsOffscreen: {0}", ele.Current.IsOffscreen);
+                Console.WriteLine("ProcessId: {0}", ele.Current.ProcessId);
+            }
+
+            int processID = 4432; //look up at your applications processID with your windows taskexplorer under processes (PID)
+            Condition yourCondition = new PropertyCondition(AutomationElement.ProcessIdProperty, processID);
+            AutomationElement mainWindow = AutomationElement.RootElement.FindFirst(TreeScope.Element | TreeScope.Children, yourCondition);
+
+            //string buttonId; your Button ID
+            //Condition yourCondition2 = new PropertyCondition(AutomationElement.AutomationId, buttonId);
+            //AutomationElement yourButton = rootElement.FindFirst(TreeScope.Element | TreeScope.Descendants, yourCondition2);
+
+            return winCollection;
+        }
+        
+    }
+}
