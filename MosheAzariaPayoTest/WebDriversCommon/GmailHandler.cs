6793fb55d91f8da18c07ffea4cc9bfbfa1a5cf91
Moshe <moshisho84@gmail.com>
2015-11-19 00:40:15 +0200

    Added payo test

---
+++ MosheAzariaPayoTest/WebDriversCommon/GmailHandler.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WebDriversCommon
+{
+    public class GmailHandler
+    {
+        public const string Homepage = "http://gmail.com";
+
+        public void SignIntoGmail(IWebDriver driver)
+        {
+            driver.Navigate().GoToUrl(Homepage);
+
+            //enter user mail miron.jango
+            // id=Email
+
+        }
+    }
+}
